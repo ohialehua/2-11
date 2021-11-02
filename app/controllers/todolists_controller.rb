af30e8f2fcc2da6f04a1d964e14e741bc51817b3
EC2 Default User <ec2-user@ip-172-31-79-219.ec2.internal>
2021-11-02 20:52:04 +0000

    編集内容の送信・保存

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -23,6 +23,13 @@ class TodolistsController < ApplicationController
     @list = List.find(params[:id])
   end
 
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolists_path(list.id)
+
+  end
+
   private
   def list_params
     params.require(:list).permit(:title, :body)
